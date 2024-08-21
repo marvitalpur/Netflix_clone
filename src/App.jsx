367e915432b5de1b5bd32264d3d90f1f5128994c
marvi <marvitalpur895@gmail.com>
2024-08-21 22:59:10 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login'
+import Plyer from './pages/Player/Player';
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/Login' element={<Login/>}/>
+        <Route path='/player/:id' element={<Plyer/>}/>
       </Routes>
   
     </div>
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import "./Player.css"
+import "./Player.css";
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Plyer = () => {
   return (
-    <div className='Player'>Plyer</div>
+    <div className='Player'>
+      <img src={back_arrow_icon} alt=''/>
+      <iframe width='90%' height='90%' src='https://www.youtube.com/embaded/PLcCy4ZCQ-4'
+      title='trailer' frameBorder='0'  allowFullScreen></iframe>
+      <div className="player_info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
   )
 }
 
